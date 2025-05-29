10b9cf3595d82d0855ee1f59e96c7bb054aeced9
JersonVitor <jersonitor2016@gmail.com>
2025-05-29 18:08:16 -0300

    att resize

--- src/python/cnn.py
+++ src/python/cnn.py
@@ -102,7 +102,7 @@ class CNNMobileNetV2(nn.Module):
 def initCNN():
     transform = transforms.Compose([
     transforms.ToPILImage(),
-    transforms.Resize((112,112)),
+    transforms.Resize((224,224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
